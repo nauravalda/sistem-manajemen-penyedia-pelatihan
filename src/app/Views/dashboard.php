a4a8710f75d67eb09d7b95c0650b4ac4daba3400
arifu11 <arifuddinachmad11@gmail.com>
2023-12-14 11:43:27 +0700

    Feat : Dashboard initial

---
+++ src/app/Views/dashboard.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Learn Sphere</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col mt-5">
+          <h1>Company Name Dashboard</h1>
+        </div>
+      </div>
+
+      <div class="row justify-content-start">
+        <div class="col-2">
+          <p>Kamis, 12 Januari 2024</p>
+        </div>
+        <div class="col-2">
+          <p>19.00 WIB</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-8">
+          <div class="card w-auto p-12">
+            <div class="card-body">
+              <h5 class="card-title">Stats</h5>
+              <!-- Kartu Inside -->
+              <div class="row">
+                <div class="card col-md mx-4" style="width: 18rem;">
+                  <div class="card-body">
+                    <h6 class="card-title">Participants / This month</h6>
+                    <!-- Info kartu -->
+                    <div class="row">
+                        <h6 class="card-text col-md align-middle">43</h6>
+                        <div class="col-md">
+                          <p class="card-text col-md">+32%</p>
+                          <p class="card-text">New participants</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <h6 class="card-text col-md align-middle">336</h6>
+                        <div class="col-md">
+                          <p class="card-text col-md">+14%</p>
+                          <p class="card-text">Total participants</p>
+                        </div>
+                    </div>
+
+                  </div>
+                </div>
+                <div class="card col-md mx-4" style="width: 18rem;">
+                  <div class="card-body">
+                    <h6 class="card-title">Ratings / This month</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+                </div>
+                <div class="card col-md mx-4" style="width: 18rem;">
+                  <div class="card-body">
+                    <h6 class="card-title">Revenue / Last 5 months</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="card w-auto">
+            <div class="card-body">
+              <h5 class="card-title">StatUpcoming Activities</h5>
+              <!-- Kartu Inside -->
+  
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <div class="card w-auto my-4">
+            <div class="card-body">
+              <h5 class="card-title">Course Overview</h5>
+              
+            </div>
+          </div>
+        </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>

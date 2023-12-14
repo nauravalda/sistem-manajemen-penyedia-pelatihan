894258adc7a128e1e425b58873ca442232c81aab
ojan <16521411@mahasiswa.itb.ac.id>
2023-12-15 04:27:34 +0700

    Repairs in color styling and html layout

--- src/app/Views/schedule.php
+++ src/app/Views/schedule.php
@@ -1,106 +1,94 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Learn Sphere</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    
-  </head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">  
+<div class="container h-full">
+  <div class="row">
+    <div class='flex flex-col my-5 gap-24'>
+        <h1 class="font-sans font-medium text-display-sm">Schedule</h1>
 
-  <body>
-    <div class="container h-full">
-      <div class="row">
-        <div class='flex flex-col my-5 gap-24'>
-            <h1 class="font-sans font-medium text-display-sm">Schedule</h1>
-    
-            <div class="row">
-              <div class='flex w-fit outline outline-1 outline-slate-400 rounded-full overflow-hidden gap-0 p-0'>
-                <button class='hover:bg-purple-100 focus:bg-purple-100 hover-text-purple-900 outline outline-1 outline-slate-400 w-[100px] px-[12px] py-[10px] gap-[8px]'>
-                  <label class='font-sans font-medium text-label-lg'>
-                    3 Days
-                  </label>
-                </button>
-                <button class='hover:bg-purple-100 focus:bg-purple-100 hover-text-purple-900 outline outline-1 outline-slate-400 w-[100px] py-[10px] gap-[8px]'>
-                  <label class='font-sans font-medium text-label-lg'>
-                    5 Days
-                  </label>
-                </button>
-                <button class='hover:bg-purple-100 focus:bg-purple-100 hover-text-purple-900 outline outline-1 outline-slate-400 w-[100px] px-[12px] py-[10px] gap-[8px]'>
-                  <label class='font-sans font-medium text-label-lg'>
-                    7 Days
-                  </label>
-                </button>
+        <div class="flex flex-row">
+          <div class='flex w-fit outline outline-1 outline-sys-light-outline-variant rounded-full overflow-hidden gap-0 p-0'>
+            <button class='hover:bg-sys-light-secondary-container focus:bg-sys-light-secondary-container hover-text-purple-900 outline outline-1 outline-sys-light-outline-variant w-[100px] px-[12px] py-[10px] gap-[8px]'>
+              <label class='font-sans font-medium text-label-lg'>
+                3 Days
+              </label>
+            </button>
+            <button class='hover:bg-sys-light-secondary-container focus:bg-sys-light-secondary-container hover-text-purple-900 outline outline-1 outline-sys-light-outline-variant w-[100px] py-[10px] gap-[8px]'>
+              <label class='font-sans font-medium text-label-lg'>
+                5 Days
+              </label>
+            </button>
+            <button class='hover:bg-sys-light-secondary-container focus:bg-sys-light-secondary-container hover-text-purple-900 outline outline-1 outline-sys-light-outline-variant w-[100px] px-[12px] py-[10px] gap-[8px]'>
+              <label class='font-sans font-medium text-label-lg'>
+                7 Days
+              </label>
+            </button>
+          </div>
+        </div>
+
+        <div class='flex flex-col w-full h-fit outline outline-1 outline-sys-light-outline-variant gap-24 p-[48px] rounded-medium'>
+
+          <div class='flex flex-col gap-24'>
+            <div class='flex flex-row w-full items-center'>
+              <div class='bg-sys-light-primary hover-text-white w-fit px-[24px] py-[10px] gap-[8px] rounded-full'>
+                <label class='font-sans font-medium text-sm text-white items-center'>
+                  17 Desember 2023
+                </label>
+              </div>
+              <div class='flex-grow border-2 border-sys-light-primary rounded-r-full'></div>
+            </div>
+            <div class='flex flex-col pl-[48px] gap-12'>
+              <div class='flex flex-col w-full h-fit bg-sys-light-surface p-[16px] gap-[8px] outline outline-1 outline-sys-light-outline-variant rounded-2xl'>
+                <h2 class='font-sans font-medium text-title-md'>Course A Pertemuan 1</h2>
+                <p class='font-sans text-body-md leading-none'>12.00 at Jl.Asap Kebakaran No. 7</p>
+              </div>
+              <div class='flex flex-col w-full h-fit bg-sys-light-surface p-[16px] gap-[8px] outline outline-1 outline-sys-light-outline-variant rounded-2xl'>
+                <h2 class='font-sans font-medium text-title-md'>Course A Pertemuan 2</h2>
+                <p class='font-sans text-body-md leading-none'>12.00 at Jl.Asap Kebakaran No. 7</p>
               </div>
             </div>
-    
-            <div class='flex flex-col w-full h-fit outline outline-1 outline-slate-400 gap-24 p-[48px] rounded-medium'>
-    
-              <div class='flex flex-col gap-24'>
-                <div class='flex flex-row w-full items-center'>
-                  <div class='bg-[#4F378B] hover-text-white w-fit px-[24px] py-[10px] gap-[8px] rounded-full'>
-                    <label class='font-sans font-medium text-sm text-white items-center'>
-                      17 Desember 2023
-                    </label>
-                  </div>
-                  <div class='flex-grow border-2 border-[#4F378B] rounded-r-full'></div>
-                </div>
-                <div class='flex flex-col pl-[48px] gap-12'>
-                  <div class='flex flex-col w-full h-fit bg-[#FEF7FF] p-[16px] gap-[8px] outline outline-1 outline-slate-400 rounded-2xl'>
-                    <h2 class='font-sans font-medium text-title-md'>Course A Pertemuan 1</h2>
-                    <p class='font-sans text-body-md leading-none'>12.00 at Jl.Asap Kebakaran No. 7</p>
-                  </div>
-                  <div class='flex flex-col w-full h-fit bg-[#FEF7FF] p-[16px] gap-[8px] outline outline-1 outline-slate-400 rounded-2xl'>
-                    <h2 class='font-sans font-medium text-title-md'>Course A Pertemuan 2</h2>
-                    <p class='font-sans text-body-md leading-none'>12.00 at Jl.Asap Kebakaran No. 7</p>
-                  </div>
-                </div>
+          </div>
+
+          <div class='flex flex-col gap-24'>
+            <div class='flex flex-row w-full items-center'>
+              <div class='bg-slate-300 hover-text-white w-fit px-[24px] py-[10px] gap-[8px] rounded-full'>
+                <label class='font-sans font-medium text-sm text-white items-center'>
+                  18 Desember 2023
+                </label>
               </div>
-    
-              <div class='flex flex-col gap-24'>
-                <div class='flex flex-row w-full items-center'>
-                  <div class='bg-slate-300 hover-text-white w-fit px-[24px] py-[10px] gap-[8px] rounded-full'>
-                    <label class='font-sans font-medium text-sm text-white items-center'>
-                      18 Desember 2023
-                    </label>
-                  </div>
-                  <div class='flex-grow border-2 border-slate-300 rounded-r-full'></div>
-                </div>
-                <div class='flex flex-col pl-[48px] gap-12'>
-                  <div class='flex flex-col w-full h-fit bg-[#FEF7FF] p-[16px] gap-[8px] outline outline-1 outline-slate-400 rounded-2xl'>
-                    <h2 class='font-sans font-medium text-title-md'>Course A Pertemuan 3</h2>
-                    <p class='font-sans text-body-md leading-none'>12.00 at Jl.Asap Kebakaran No. 7</p>
-                  </div>
-                  <div class='flex flex-col w-full h-fit bg-[#FEF7FF] p-[16px] gap-[8px] outline outline-1 outline-slate-400 rounded-2xl'>
-                    <h2 class='font-sans font-medium text-title-md'>Course A Pertemuan 4</h2>
-                    <p class='font-sans text-body-md leading-none'>12.00 at Jl.Asap Kebakaran No. 7</p>
-                  </div>
-                </div>
+              <div class='flex-grow border-2 border-slate-300 rounded-r-full'></div>
+            </div>
+            <div class='flex flex-col pl-[48px] gap-12'>
+              <div class='flex flex-col w-full h-fit bg-sys-light-surface p-[16px] gap-[8px] outline outline-1 outline-sys-light-outline-variant rounded-2xl'>
+                <h2 class='font-sans font-medium text-title-md'>Course A Pertemuan 3</h2>
+                <p class='font-sans text-body-md leading-none'>12.00 at Jl.Asap Kebakaran No. 7</p>
               </div>
-              
-              <div class='flex flex-col gap-24'>
-                <div class='flex flex-row w-full items-center'>
-                  <div class='bg-slate-300 hover-text-white w-fit px-[24px] py-[10px] gap-[8px] rounded-full'>
-                    <label class='font-sans font-medium text-sm text-white items-center'>
-                      19 Desember 2023
-                    </label>
-                  </div>
-                  <div class='flex-grow border-2 border-slate-300 rounded-r-full'></div>
-                </div>
-                <div class='flex flex-col pl-[48px] gap-12'>
-                  <div class='flex flex-col w-full h-fit bg-[#FEF7FF] p-[16px] gap-[8px] outline outline-1 outline-slate-400 rounded-2xl'>
-                    <h2 class='font-sans font-medium text-title-md'>Course A Pertemuan 5</h2>
-                    <p class='font-sans text-body-md leading-none'>12.00 at Jl.Asap Kebakaran No. 7</p>
-                  </div>
-                  <div class='flex flex-col w-full h-fit bg-[#FEF7FF] p-[16px] gap-[8px] outline outline-1 outline-slate-400 rounded-2xl'>
-                    <h2 class='font-sans font-medium text-title-md'>Course A Pertemuan 6</h2>
-                    <p class='font-sans text-body-md leading-none'>12.00 at Jl.Asap Kebakaran No. 7</p>
-                  </div>
-                </div>
+              <div class='flex flex-col w-full h-fit bg-sys-light-surface p-[16px] gap-[8px] outline outline-1 outline-sys-light-outline-variant rounded-2xl'>
+                <h2 class='font-sans font-medium text-title-md'>Course A Pertemuan 4</h2>
+                <p class='font-sans text-body-md leading-none'>12.00 at Jl.Asap Kebakaran No. 7</p>
+              </div>
+            </div>
+          </div>
+          
+          <div class='flex flex-col gap-24'>
+            <div class='flex flex-row w-full items-center'>
+              <div class='bg-slate-300 hover-text-white w-fit px-[24px] py-[10px] gap-[8px] rounded-full'>
+                <label class='font-sans font-medium text-sm text-white items-center'>
+                  19 Desember 2023
+                </label>
+              </div>
+              <div class='flex-grow border-2 border-slate-300 rounded-r-full'></div>
+            </div>
+            <div class='flex flex-col pl-[48px] gap-12'>
+              <div class='flex flex-col w-full h-fit bg-sys-light-surface p-[16px] gap-[8px] outline outline-1 outline-sys-light-outline-variant rounded-2xl'>
+                <h2 class='font-sans font-medium text-title-md'>Course A Pertemuan 5</h2>
+                <p class='font-sans text-body-md leading-none'>12.00 at Jl.Asap Kebakaran No. 7</p>
+              </div>
+              <div class='flex flex-col w-full h-fit bg-sys-light-surface p-[16px] gap-[8px] outline outline-1 outline-sys-light-outline-variant rounded-2xl'>
+                <h2 class='font-sans font-medium text-title-md'>Course A Pertemuan 6</h2>
+                <p class='font-sans text-body-md leading-none'>12.00 at Jl.Asap Kebakaran No. 7</p>
               </div>
             </div>
           </div>
         </div>
+      </div>
     </div>
-  </body>
-</html>
+</div>

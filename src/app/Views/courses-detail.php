27ae0081180dadfa94642d23767181227347794f
arifu11 <arifuddinachmad11@gmail.com>
2023-12-18 10:51:40 +0700

    Perbaikan tombol back dan perapihan

--- src/app/Views/courses-detail.php
+++ src/app/Views/courses-detail.php
@@ -15,7 +15,7 @@
           <!-- Back button -->
           <div class="w-full self-start text-light-primary gap-2">
             <button onclick="window.location.href = '/courses';"
-              class="flex items-center text-label-lg font-medium font-['Roboto'] leading-tight gap-2 p-2 hover:bg-fuschia-100">
+              class="flex items-center text-label-lg font-semibold leading-tight gap-2 p-2 hover:bg-fuschia-100">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
                 <path d="M15 8.25H5.8725L10.065 4.0575L9 3L3 9L9 15L10.0575 13.9425L5.8725 9.75H15V8.25Z"
                   fill="#6750A4" />
